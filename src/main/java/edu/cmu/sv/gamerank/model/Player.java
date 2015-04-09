841599d6c8407c5d5a0d1b6477f10da5accb9396
VennoFang <octtty@gmail.com>
2015-04-08 21:04:29 -0700

    Implement elorating

--- src/main/java/edu/cmu/sv/gamerank/model/Player.java
+++ src/main/java/edu/cmu/sv/gamerank/model/Player.java
@@ -2,19 +2,30 @@ package edu.cmu.sv.gamerank.model;
 
 public class Player {
 	private String name;
-	private Long score;
-	
+	private int score;
+	private static final int K = 32;
+//	private Double exp;
+//	public Double getExp(Long scoreB) {
+//		return exp = 1/(1 + Math.pow(10, (scoreB - score)/400));
+//	}
+	public void rating(int newScore, int gotScore) {
+		Double  exp = 1/(1 + Math.pow(10, (newScore - score)/400));
+		score = (int) (score + K * (gotScore - exp));
+	}
+
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Long getScore() {
+	public int getScore() {
 		return score;
 	}
-	public void setScore(Long score) {
+	public void setScore(int score) {
 		this.score = score;
 	}
+	
+	
 	
 }
---
+++ src/main/java/edu/cmu/sv/gamerank/util/EloRating.java
@@ -0,0 +1,5 @@
+package edu.cmu.sv.gamerank.util;
+
+public class EloRating {
+
+}
